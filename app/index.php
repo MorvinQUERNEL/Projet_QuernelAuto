f1fcce78db6bfe38a070b40ee9170ffcd145045a
Morvin Quernel <quernel.morvin@icloud.com>
2025-05-07 13:36:42 +0200

    maj_css

--- app/index.php
+++ app/index.php
@@ -1,7 +1,135 @@
-<?php
-ini_set("SMTP", "mailhog:1025");
-ini_set("sendmail_from", "YOURMAIL@gmail.com");
-
-phpinfo();
-
-mail('a@b.c', 'joli mail', 'Joli message');
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Quernel Auto</title>
+        <link rel="stylesheet" href="css/style.css?v1">
+        <link rel="icon" href="images/logo_quernel_auto.png" type="image/png">
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+        <header>
+            <div class="navbar">
+                <div class="container_logo">
+                    <img src="images/logo_quernel_auto.png" alt="logo quernel auto">
+                </div>
+                <h1>Quernel Auto</h1>
+                <div class="burger-button" onclick="">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </header>
+        <main>
+            <section id="index_accueil">
+                <div>
+                    <h2>Quernel Auto</h2>
+                </div>
+                <div class="text_accueil">
+                    <p>
+                        QUERNEL AUTO simplifie l'import-export de voitures, offrant des solutions fiables et transparentes pour l'achat et la vente de véhicules à l'international. Avec une expertise reconnue et un réseau mondial, nous vous accompagnons à chaque étape pour rendre vos transactions automobiles fluides et sécurisées.
+                    </p>
+                </div>
+                    <button>remplire un formulaire</button>
+            </section>
+            <section id="index_etapes">
+                <article>
+                    <div>
+                        <h3>ETAPE 1</h3>
+                        <p>Recherche Personnalisée</p>
+                    </div>
+                    <div>
+                        <img src="images/etape1.jpg" alt="">
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <img src="images/etape2.avif" alt="">
+                    </div>
+                    <div>
+                    <h3>ETAPE 2</h3>
+                    <p>Négociation et Achat Sécurisé</p>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <h3>ETAPE 3</h3>
+                        <p>Transport et Formalités</p>
+                    </div>
+                    <div>
+                        <img src="images/etape3.jpg" alt="">
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <img src="images/etape4.jpg" alt="">
+                    </div>
+                    <div>
+                        <h3>ETAPE 4</h3>
+                        <p>Homologation et Immatriculation</p>
+                    </div>
+                </article>
+            </section>
+            <section id="index_carouselle">
+                <h2>nos voiturex exporté en 2025</h2>
+                <div class="contener_carouselle">
+                    <figure>
+                        <img src="" alt="">
+                        <figcaption></figcaption>
+                    </figure>
+                </div>
+            </section>
+            <section id="index_form">
+                <form action="" method="post">
+                    <div>
+                        <label for="index_form_nom">Nom:</label>
+                        <input type="text" id="index_form_nom">
+                    </div>
+                    <div>
+                        <label for="index_form_prenom">Prenom:</label>
+                        <input type="text" id="index_form_prenom">
+                    </div>
+                    <div>
+                        <label for="index_form_email">Email:</label>
+                        <input type="email" id="index_form_email">
+                    </div>
+                    <div>
+                        <label for="index_form_demande">Pour quel type de demande:</label>
+                        <select id="index_form_demande" name="demande">
+                            <option value="import">import</option>
+                            <option value="devis">devis</option>
+                            <option value="renseignement">renseignement divers</option>
+                        </select>
+                    </div>
+                    <button type="submit">Je veux etre contacté</button>
+                </form>
+            </section>
+        </main>
+        <footer>
+            <a href="">
+            <div class="container_logo">    
+                <img src="images/logo_quernel_auto.png" alt="logo quernel auto">  
+            </div>
+            </a>
+            <a href="">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px" fill="#FF6B00">
+                    <path d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z"/>
+                </svg>
+            </a>
+            <a href="">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px" fill="#FF6B00">
+                    <path d="M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"/>
+                </svg>
+            </a>
+            <a href="">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px" fill="#FF6B00">
+                    <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"/>
+                </svg>
+            </a>
+        </footer>
+    </body>
+</html>
